9260787f6bf0bd22641f76213ac722cc3f5519fa
Haxonek <thomash1996@live.com>
2016-11-13 12:30:07 -0600

    EBook creates only one type of ebook rn, BUG: can't find main method

--- src/EBook.java
+++ src/EBook.java
@@ -1,22 +1,27 @@
-import java.util.List;
-import java.util.ArrayList;
+//import java.util.List;
+//import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
+//import java.nio.file.Files;
 
 public class EBook {
 	
-	Scanner in = new Scanner(System.in);
+//	Scanner in = new Scanner(System.in);
+//	List<Section> chapters = new ArrayList<Section>();
+//	OverHead overHead;
 	
-	List<Section> chapters = new ArrayList<Section>();
-	OverHead overHead;
-	File book;
-	
-	public void main (String[] args) {
+	public static void main(String args[]) {
 		
-		System.out.println("Start of Ebook complimation.");
+		Scanner in = new Scanner(System.in);
+//		List<Section> chapters = new ArrayList<Section>();
+		OverHead overHead;
+		
+		System.out.println("EBook class started");
+		
+		
+		
 		
 		// Read in static information
 		System.out.print("Book Title: ");
@@ -25,30 +30,36 @@ public class EBook {
 		String author = in.nextLine();
 		
 		overHead = new OverHead(title, author);
+		Section sections = new Section();
 		
 		// create a new section for each arg.  For now just use own text
 		
 		// add each to list of chapters
 		
 		// create ebook
-		book = createEbook();
+		createEbook(overHead);
+		createSections(sections);
 		
 		// zip up ebook
 		zipBook();
 		
-		// rename ebook file
-		int PerLoc = book.getName().indexOf('.');
+//		// rename ebook file
+//		int PerLoc = book.getName().indexOf('.');
+//		// I removed book and can't figure out what type it was :/
+		// Wait it was File book; may have to do some other way
 		
-		// copies it to the new file name, minus old extention
-		try {
-			File newFile = new File(book.getParent(), book.getName().substring(0, PerLoc) + ".epub");
-			Files.move(book.toPath(), newFile.toPath());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+//		// copies it to the new file name, minus old extention
+//		try {
+//			File newFile = new File(book.getParent(), book.getName().substring(0, PerLoc) + ".epub");
+//			Files.move(book.toPath(), newFile.toPath());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+		
+		in.close();
 	}
 
-	public File createEbook() {
+	public static void createEbook(OverHead oh) {
 
 		try {
 			// Create a folder OEPBS, META-INF, and file mimetype
@@ -60,39 +71,55 @@ public class EBook {
 
 			// (contents: "application/epub+zip")
 			File mimetype = new File("EPUB/mimetype");
-
 			// now we add the content:
 			FileWriter f = new FileWriter(mimetype);
 			f.write("application/epub+zip");
 			f.close();
 			
+			// create other files
+			oh.makeOverhead();
+			
+			// create the three chapters; will automate later
+			
+			
 			// Now we add files to META_INF
-			return overHead.makeContainer();
+//			return overHead.makeContainer();
 
+			
+			
 		} catch (Exception ie) {
 			ie.printStackTrace();
-			return null;
+//			return null;
 		}
 	}
 	
-	public boolean zipBook() {
+	public static void createSections(Section s) {
+		try {
+			// WILL BE DIFFERENT LATER
+			s.makeChapter();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static boolean zipBook() {
 		return false;
 	}
 	
-	public void setAuthor(String author) {
-		overHead.setAuthor(author);
-	}
-	
-	public void setTitle(String title) {
-		overHead.setTitle(title);
-	}
-	
-	public String getTitle() {
-		return overHead.getTitle();
-	}
-
-	public String getAuthor() {
-		return overHead.getAuthor();
-	}
+//	public void setAuthor(String author) {
+//		overHead.setAuthor(author);
+//	}
+//	
+//	public void setTitle(String title) {
+//		overHead.setTitle(title);
+//	}
+//	
+//	public String getTitle() {
+//		return overHead.getTitle();
+//	}
+//
+//	public String getAuthor() {
+//		return overHead.getAuthor();
+//	}
 	
 }
--- src/OverHead.java
+++ src/OverHead.java
@@ -16,27 +16,52 @@ public class OverHead {
 		this.epubName = title.trim();
 	}
 	
-	public File makeCSS() throws IOException {
-		File css = new File("h1 {text-align: center;}");
-		return css;
+	public void makeOverhead() {
+		try {
+			makeContent();
+			makeCSS();
+			makeToc();
+			makeTox();
+			makeContainer();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("General file created.");
 	}
 	
-	public File makeContent() throws IOException {
-		return null;
+	public void makeCSS() throws IOException {
+		File css = new File("EPUB/OEPBS/CSS/template.css");
+		FileWriter f = new FileWriter(css);
+		f.write("h1 {text-align: center;}");
+		f.close();
+		css.createNewFile();
 	}
 	
-	public File makeToc() throws IOException {
-		
-		File package_opf = new File("EPUB/EPUB/package.opf");
+	public void makeContent() throws IOException {
+		File package_opf = new File("EPUB/OEPBS/package.opf");
 		FileWriter f = new FileWriter(package_opf);
 		f.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<package xmlns=\"http://www.idpf.org/2007/opf\" version=\"3.1\" xml:lang=\"en\" unique-identifier=\"pub-id\"\n  prefix=\"dc: http://purl.org/dc/elements/1.1/\">\n\n  <metadata xmlns:dc=\"http://purl.org/dc/elements/1.1/\">\n\n    <dc:title id=\"title\">Moby-Dick</dc:title>\n    <dc:creator id=\"creator\" file-as=\"Melville, Herman\">Herman Melville</dc:creator>\n    <dc:identifier id=\"pub-id\">idpf.epub31.samples.moby-dick.xhtml</dc:identifier>\n    <dc:language>en-US</dc:language>\n    <meta property=\"dcterms:modified\">2016-02-05T14:40:00Z</meta>\n\n    <dc:publisher>Harper &amp; Brothers, Publishers</dc:publisher>\n    <meta property=\"dc:contributor\">Dave Cramer</meta>\n\n    <!--MEDIA OVERLAY METADATA-->\n    <meta property=\"media:duration\">0:23:23.500</meta>\n    <meta property=\"media:narrator\">Stuart Wills</meta>\n\n    <meta property=\"media:active-class\">-epub-media-overlay-active</meta>\n  </metadata>\n  <manifest>\n    <item id=\"font.stix.regular\" href=\"fonts/STIXGeneral.otf\" media-type=\"application/vnd.ms-opentype\"/>\n    <item id=\"font.stix.italic\" href=\"fonts/STIXGeneralItalic.otf\" media-type=\"application/vnd.ms-opentype\"/>\n    <item id=\"font.stix.bold\" href=\"fonts/STIXGeneralBol.otf\" media-type=\"application/vnd.ms-opentype\"/>\n    <item id=\"font.stix.bold.italic\" href=\"fonts/STIXGeneralBolIta.otf\" media-type=\"application/vnd.ms-opentype\"/>\n\n    <item id=\"chapter_001_overlay\" href=\"chapter_001_overlay.smil\" duration=\"0:14:20.500\" media-type=\"application/smil+xml\"/>\n    <item id=\"chapter_002_overlay\" href=\"chapter_002_overlay.smil\" duration=\"0:09:03.000\" media-type=\"application/smil+xml\"/>\n\n    <item id=\"chapter_001_audio\" href=\"audio/mobydick_001_002_melville.mp4\" media-type=\"audio/mp4\"/>\n\n    <item id=\"cover-image\" properties=\"cover-image\" href=\"images/9780316000000.jpg\" media-type=\"image/jpeg\"/>\n\n    <item id=\"style\" href=\"css/stylesheet.css\" media-type=\"text/css\"/>\n\n    <item id=\"toc\" properties=\"nav\" href=\"toc.xhtml\" media-type=\"application/xhtml+xml\"/>\n    <item id=\"copyright\" href=\"copyright.xhtml\" media-type=\"application/xhtml+xml\"/>\n    <item id=\"titlepage\" href=\"titlepage.xhtml\" media-type=\"application/xhtml+xml\"/>\n    <item id=\"cover\" href=\"cover.xhtml\" media-type=\"application/xhtml+xml\"/>\n    <item id=\"aMoby-Dick_FE_title_page\" href=\"images/Moby-Dick_FE_title_page.jpg\" media-type=\"image/jpeg\"/>\n    <item id=\"xpreface_001\" href=\"preface_001.xhtml\" media-type=\"application/xhtml+xml\"/>\n    <item id=\"xintroduction_001\" href=\"introduction_001.xhtml\" media-type=\"application/xhtml+xml\"/>\n    <item id=\"xepigraph_001\" href=\"epigraph_001.xhtml\" media-type=\"application/xhtml+xml\"/>\n    <item id=\"xchapter_001\" href=\"chapter_001.xhtml\" media-type=\"application/xhtml+xml\" media-overlay=\"chapter_001_overlay\"/>\n    <item id=\"xchapter_002\" href=\"chapter_002.xhtml\" media-type=\"application/xhtml+xml\" media-overlay=\"chapter_002_overlay\"/>\n    <item id=\"xchapter_003\" href=\"chapter_003.xhtml\" media-type=\"application/xhtml+xml\"/>\n    <item id=\"xchapter_004\" href=\"chapter_004.xhtml\" media-type=\"application/xhtml+xml\"/>\n    <item id=\"xchapter_005\" href=\"chapter_005.xhtml\" media-type=\"application/xhtml+xml\"/>\n    <item id=\"brief-toc\" href=\"toc-short.xhtml\" media-type=\"application/xhtml+xml\"/>\n\n    <!-- \n    <item id=\"ncx\" href=\"toc.ncx\" media-type=\"application/x-dtbncx+xml\"/>\n -->\n\n  </manifest>\n  <spine>\n    <itemref idref=\"cover\" linear=\"no\"/>\n    <itemref idref=\"titlepage\" linear=\"yes\"/>\n    <itemref idref=\"brief-toc\" linear=\"yes\"/>\n    <itemref linear=\"yes\" idref=\"xpreface_001\"/>\n    <itemref linear=\"yes\" idref=\"xintroduction_001\"/>\n    <itemref linear=\"yes\" idref=\"xepigraph_001\"/>\n    <itemref linear=\"yes\" idref=\"xchapter_001\"/>\n    <itemref linear=\"yes\" idref=\"xchapter_002\"/>\n    <itemref linear=\"yes\" idref=\"xchapter_003\"/>\n    <itemref linear=\"yes\" idref=\"xchapter_004\"/>\n    <itemref linear=\"yes\" idref=\"xchapter_005\"/>\n    <itemref idref=\"copyright\" linear=\"yes\"/>\n    <itemref idref=\"toc\" linear=\"no\"/>\n  </spine>\n</package>\n");
 		f.close();
 		package_opf.createNewFile();
+	}
+	
+	public void makeToc() throws IOException {
 		
-		return null;
+		File toc_xhtml = new File("EPUB/OEPBS/toc.xhtml");
+		FileWriter f = new FileWriter(toc_xhtml);
+		f.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:epub=\"http://www.idpf.org/2007/ops\">\n<head>\n<title>toc.xhtml</title>\n<link href=\"template.css\" rel=\"stylesheet\" type=\"text/css\" />\n</head>\n<body>\n    <nav id=\"toc\" epub:type=\"toc\">\n        <h1 class=\"frontmatter\">Table of Contents</h1>\n        <ol class=\"contents\">\n               <li><a href=\"chapter_1.xhtml\">Chapter 1</a></li>\n               <li><a href=\"chapter_2.xhtml\">Chapter 2</a></li>\n               <li><a href=\"copyright.xhtml\">Copyright</a></li>\n        </ol>\n    </nav>\n</body>\n</html>");
+		f.close();
+		toc_xhtml.createNewFile();
+		
 	}
 	
 	public File makeTox() throws IOException {
+		File toc_ncx = new File("EPUB/OEPBS/toc.ncx");
+		FileWriter f = new FileWriter(toc_ncx);
+		f.write("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<ncx version=\"2005-1\" xml:lang=\"en\" xmlns=\"http://www.daisy.org/z3986/2005/ncx/\">\n\n<head>\n    <meta name=\"dtb:uid\" content=\"isbn\"/>\n    <meta name=\"dtb:depth\" content=\"1\"/>\n</head>\n\n<docTitle>\n    <text>Sample .epub Book</text>\n</docTitle>\n\n<navMap>\n    <navPoint id=\"chapter_1\" playOrder=\"1\">\n        <navLabel><text>Chapter 1</text></navLabel>\n        <content src=\"chapter_1.xhtml\" />\n    </navPoint>\n</navMap>\n\n<navMap>\n    <navPoint id=\"chapter_2\" playOrder=\"2\">\n        <navLabel><text>Chapter 2</text></navLabel>\n        <content src=\"chapter_2.xhtml\" />\n    </navPoint>\n</navMap>\n\n<navMap>\n    <navPoint id=\"copyright\" playOrder=\"3\">\n        <navLabel><text>Copyright</text></navLabel>\n        <content src=\"copyright.xhtml\" />\n    </navPoint>\n</navMap>\n\n</ncx>");
+		f.close();
+		toc_ncx.createNewFile();
+		
 		return null;
 	}
 
