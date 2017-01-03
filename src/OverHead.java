4ad4765168a31c1570e570db08892fa58da1195c
Haxonek <thomash1996@live.com>
2017-01-02 23:23:26 -0600

    Fixed bug (duplicate file in opf) ppreventing epub from running on iBooks.  Minor text fixes.

--- src/OverHead.java
+++ src/OverHead.java
@@ -48,7 +48,7 @@ public class OverHead {
 	 */
 	public void makeOverhead(int numSections) {
 		try {
-			makeContent(numSections);
+			makeOpf(numSections);
 			makeCSS();
 			makeTitlePage(numSections);
 			makeToc(numSections);
@@ -68,15 +68,15 @@ public class OverHead {
 		// make css template
 		File css = new File("EPUB/OEPBS/CSS/template.css");
 		FileWriter f = new FileWriter(css);
-		f.write("h1 {\n\ttext-align: center;\n\tfont-size: 1.6em;\n}\n\n.frontmatter {\n  font-family: \"Helvetica\", sans-serif;\n  font-weight: lighter;\n}\n\nli{\n  list-style: none;\n  font-family: \"Helvetica\", sans-serif;\n  line-height: 1.35em;\n  font-size: 1.1em;\n  text-decoration: none;\n  font-weight: lighter;\n}\n\np {line-height: 1.53em; font-size: 1em;}\na {text-decoration: none;}\na:link    { color: #00d9ff }\na:visited { color: #00d9ff }\n.book_title {font-size: 3em;width: 80%;margin: 100% auto;border-bottom: 4px solid #cdcdcd;font-family: serif;}\n.author_name {text-align: center;color: #333; font-family: sans-serif;}\n.chapter_count {font-size: .8em;text-align: center;color: #333;font-family: sans-serif;}");
+		f.write("h1, h2, h3, h4, h5 {\n\tcolor: #666;\n	font-family: sans-serif;\n	font-size: 2.2em;\n	text-align: right;\n}\n\nh6 {\n	color: #666;\n	font-family: sans-serif;\n	font-size: 2.2em;\n}\n\nh1 { font-size: 2.4em; }\nh2 { font-size: 2.2em; }\nh3 { font-size: 2em; }\nh4 { font-size: 1.4em; }\nh5 { font-size: 1.2em; }\nh6 { font-size: 1em; }\n\n\np {\n	color: #444;\n	font-family: serif;\n	font-size: 1.3em;\n}\n\na {\n	text-decoration: none;\n	color: #1EAEDB;\n}\n\nli {\n	list-style: upper-roman;\n	color: #444;\n	font-family: sans-serif;\n	font-size: 1.2em;\n	line-height: 1.4em;\n}\n\n.book_title {\n	border-bottom: .2em #666 solid;\n}");
 		f.close();
 		css.createNewFile();
 	}
 	
-	private void makeContent(int n) throws IOException {
+	private void makeOpf(int n) throws IOException {
 		
 		// generate the list of chapters in a string
-		String manifest_links = "", spine_links = "";
+		String manifest_links = "", spine_links = "\t\t<itemref idref=\"titlepage\" />\n\t\t<itemref idref=\"toc\" />\n";
 		for (int i = 1; i <= n; ++i) {
 			manifest_links += "\t\t<item id=\"chapter_" + i + "\" href=\"chapter_" + i + ".xhtml\" media-type=\"application/xhtml+xml\" />\n";
 			spine_links += "\t\t<itemref idref=\"chapter_" + i + "\" />\n";
@@ -85,7 +85,7 @@ public class OverHead {
 		// create file and add chapters to string, then add to content.opf
 		File content_opf = new File("EPUB/OEPBS/content.opf");
 		FileWriter f = new FileWriter(content_opf);
-		f.write("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<package xmlns=\"http://www.idpf.org/2007/opf\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" unique-identifier=\"db-id\" version=\"3.0\">\n\n\t<metadata>\n	    <dc:title id=\"t1\">" + title + "</dc:title>\n	    <dc:creator opf:role=\"aut\">" + author + "</dc:creator>\n	    <dc:identifier id=\"db-id\">isbn</dc:identifier>\n	    <meta property=\"dcterms:modified\">" + LocalDateTime.now() + "</meta>\n	    <dc:language>en</dc:language>\n	</metadata>\n\n	<manifest>\n	    <item id=\"toc\" properties=\"nav\" href=\"toc.xhtml\" media-type=\"application/xhtml+xml\" />\n	    <item id=\"ncx\" href=\"toc.ncx\" media-type=\"application/x-dtbncx+xml\" />\n	    <item id=\"template_css\" href=\"template.css\" media-type=\"text/css\" />\n	    <item id=\"toc\" href=\"toc.xhtml\" media-type=\"application/xhtml+xml\" />\n\t\t<item id=\"titlepage\" href=\"titlepage.xhtml\" media-type=\"application/xhtml+xml\" />\n" + manifest_links + "	    <item id=\"copyright\" href=\"copyright.xhtml\" media-type=\"application/xhtml+xml\" />\n	</manifest>\n\n	<spine toc=\"ncx\">\n		<itemref idref=\"toc\" />\n\t\t<itemref idref=\"titlepage\" />\n" + spine_links + "	    <itemref idref=\"copyright\" />\n	</spine>\n\n</package>");
+		f.write("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<package xmlns=\"http://www.idpf.org/2007/opf\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" unique-identifier=\"db-id\" version=\"3.0\">\n\n\t<metadata>\n	    <dc:title id=\"t1\">" + title + "</dc:title>\n	    <dc:creator opf:role=\"aut\">" + author + "</dc:creator>\n	    <dc:identifier id=\"db-id\">isbn</dc:identifier>\n	    <meta property=\"dcterms:modified\">" + LocalDateTime.now() + "</meta>\n	    <dc:language>en</dc:language>\n	</metadata>\n\n	<manifest>\n	    <item id=\"toc\" properties=\"nav\" href=\"toc.xhtml\" media-type=\"application/xhtml+xml\" />\n	    <item id=\"ncx\" href=\"toc.ncx\" media-type=\"application/x-dtbncx+xml\" />\n	    <item id=\"template_css\" href=\"template.css\" media-type=\"text/css\" />\n\t\t<item id=\"titlepage\" href=\"titlepage.xhtml\" media-type=\"application/xhtml+xml\" />\n" + manifest_links + "	    <item id=\"copyright\" href=\"copyright.xhtml\" media-type=\"application/xhtml+xml\" />\n	</manifest>\n\n	<spine toc=\"ncx\">\n" + spine_links + "	    <itemref idref=\"copyright\" />\n	</spine>\n\n</package>");
 		f.close();
 		content_opf.createNewFile();
 	}
@@ -99,7 +99,7 @@ public class OverHead {
 		
 		File toc_xhtml = new File("EPUB/OEPBS/toc.xhtml");
 		FileWriter f = new FileWriter(toc_xhtml);
-		f.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:epub=\"http://www.idpf.org/2007/ops\">\n<head>\n<title>toc.xhtml</title>\n<link href=\"CSS/template.css\" rel=\"stylesheet\" type=\"text/css\" />\n</head>\n<body>\n    <nav id=\"toc\" epub:type=\"toc\">\n        <h1 class=\"frontmatter\">Table of Contents</h1>\n        <ol class=\"contents\">\n\t\t\t<li><a href=\"titlePage.xhtml\">Title page</a></li>\n\t\t\t" + toc_links + "\t\t\t<li><a href=\"copyright.xhtml\">Copyright</a></li>\n        </ol>\n    </nav>\n</body>\n</html>");
+		f.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:epub=\"http://www.idpf.org/2007/ops\">\n<head>\n<title>toc.xhtml</title>\n<link href=\"CSS/template.css\" rel=\"stylesheet\" type=\"text/css\" />\n</head>\n<body>\n    <nav id=\"toc\" epub:type=\"toc\">\n        <h1 class=\"frontmatter\">Table of Contents</h1>\n        <ol class=\"contents\">\n\t\t\t<li><a href=\"titlePage.xhtml\">Title page</a></li>\n" + toc_links + "\t\t\t<li><a href=\"copyright.xhtml\">Copyright</a></li>\n        </ol>\n    </nav>\n</body>\n</html>");
 		f.close();
 		toc_xhtml.createNewFile();
 	}
@@ -118,9 +118,9 @@ public class OverHead {
 		
 		for (int i = 2; i <= n+1; ++i) {
 			toc_tags += "<navMap>\n";
-			toc_tags += "\t<navPoint id=\"chapter_" + i + "\" playOrder=" + i + ">\n";
-			toc_tags += "\t\t<navLabel><text>Chapter " + i + "</text></navLabel>\n";
-			toc_tags += "\t\t<content src=\"chapter_" + i + ".xhtml\" />\n";
+			toc_tags += "\t<navPoint id=\"chapter_" + (i - 1) + "\" playOrder=" + i + ">\n";
+			toc_tags += "\t\t<navLabel><text>Chapter " + (i - 1) + "</text></navLabel>\n";
+			toc_tags += "\t\t<content src=\"chapter_" + (i - 1) + ".xhtml\" />\n";
 			toc_tags += "\t</navPoint>\n";
 			toc_tags += "</navMap>\n\n";
 		}
@@ -135,7 +135,7 @@ public class OverHead {
 		
 		File toc_ncx = new File("EPUB/OEPBS/toc.ncx");
 		FileWriter f = new FileWriter(toc_ncx);
-		f.write("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<ncx version=\"2005-1\" xml:lang=\"en\" xmlns=\"http://www.daisy.org/z3986/2005/ncx/\">\n\n<head>\n    <meta name=\"dtb:uid\" content=\"isbn\"/>\n    <meta name=\"dtb:depth\" content=\"1\"/>\n</head>\n\n<docTitle>\n    <text>" + title + "</text>\n</docTitle>\n\n" + toc_tags + "<navMap>\n    <navPoint id=\"copyright\" playOrder=\"3\">\n        <navLabel><text>Copyright</text></navLabel>\n        <content src=\"copyright.xhtml\" />\n    </navPoint>\n</navMap>\n\n</ncx>");
+		f.write("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<ncx version=\"2005-1\" xml:lang=\"en\" xmlns=\"http://www.daisy.org/z3986/2005/ncx/\">\n\n<head>\n    <meta name=\"dtb:uid\" content=\"isbn\"/>\n    <meta name=\"dtb:depth\" content=\"1\"/>\n</head>\n\n<docTitle>\n    <text>" + title + "</text>\n</docTitle>\n\n" + toc_tags + "<navMap>\n    <navPoint id=\"copyright\" playOrder=\"" + (n + 2) + "\">\n        <navLabel><text>Copyright</text></navLabel>\n        <content src=\"copyright.xhtml\" />\n    </navPoint>\n</navMap>\n\n</ncx>");
 		f.close();
 		toc_ncx.createNewFile();
 	}
@@ -149,7 +149,7 @@ public class OverHead {
 	private void makeCopyright() throws IOException {
 		File container = new File("EPUB/OEPBS/copyright.xhtml");
 		FileWriter f = new FileWriter(container);
-		f.write("<h1>Copyright</h1><p>This is where copyright information could potentually go.</p>");
+		f.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:epub=\"http://www.idpf.org/2007/ops\">\n<head>\n\t<title>Copyright</title>\n	<link href=\"CSS/template.css\" rel=\"stylesheet\" type=\"text/css\" />\n</head>\n<body>\n	<p>Copyright " + author + ", " + LocalDateTime.now().getYear() + "</p>\n</body>\n</html>\n");
 		f.close();
 	}
 	
@@ -162,7 +162,7 @@ public class OverHead {
 	private void makeTitlePage(int numsections) throws IOException {
 		File container = new File("EPUB/OEPBS/titlepage.xhtml");
 		FileWriter f = new FileWriter(container);
-		f.write("<?xml version=\"1.0\" encoding=\"utf-8\"?><html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:epub=\"http://www.idpf.org/2007/ops\"><head><title>" + title + "</title><link href=\"CSS/template.css\" rel=\"stylesheet\" type=\"text/css\" /></head><body><h1 class=\"book_title\">" + title + "</h1><h3 class=\"author_name\">" + author + "</h3><h5 class=\"chapter_count\">" + numsections + " Chapters</h5></body></html>");
+		f.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:epub=\"http://www.idpf.org/2007/ops\">\n<head>\n\t<title>" + title + "</title>\n	<link href=\"CSS/template.css\" rel=\"stylesheet\" type=\"text/css\" />\n</head>\n<body>\n	<h1 class=\"book_title\">" + title + "</h1>\n	<h3 class=\"author_name\">" + author + "</h3>\n	<h5 class=\"chapter_count\">" + numsections + " Chapters</h5>\n</body>\n</html>\n");
 		f.close();
 	}
 
